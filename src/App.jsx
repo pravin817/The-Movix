dc01b17f6b94dffdef4217dfbd5b570e6738d24a
Pravin Mhaske <pravinmhaske817@gmail.com>
2023-08-31 18:23:46 +0530

    TMDB API setup - API Testing done successfully.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,18 @@
+import { useEffect } from "react"
+import { fetchDataFromApi } from "./utils/api"
+
 function App() {
+
+  useEffect(() => {
+    ApiTesting();
+  }, [])
+
+  const ApiTesting = ()=>{
+    fetchDataFromApi("/movie/popular").then((response)=>{
+      console.log(response)
+    })
+  }
+  
 
   return (
     <>
