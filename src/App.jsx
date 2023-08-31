1666431e34245da0f4f0182010c229b57d827847
Pravin Mhaske <pravinmhaske817@gmail.com>
2023-08-31 19:20:25 +0530

    Pages and Components created successfully.

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,15 @@ import { fetchDataFromApi } from "./utils/api";
 import { useSelector, useDispatch } from "react-redux";
 import { getApiConfiguration } from "./store/homeSlice";
 
+// import the components
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
+import Home from "./pages/home/Home";
+import Details from "./pages/details/Details";
+import SearchResult from "./pages/searchResult/SearchResult";
+import Explore from "./pages/explore/Explore";
+import PageNotFound from "./pages/404/PageNotFound";
+
 function App() {
   // To store the data in Store
   const dispatch = useDispatch();
@@ -26,7 +35,7 @@ function App() {
   return (
     <>
       <div className="App">
-        App
+        Hello
         {url?.total_pages}
       </div>
     </>
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,10 @@
+import HeroBanner from "./heroBanner/HeroBanner";
+import "./style.scss";
+
+const Home = () => {
+  return <div className="homePage">
+    <HeroBanner/>
+  </div>;
+};
+
+export default Home;
