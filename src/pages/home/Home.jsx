d2f4336ec7f5d64e36749b7be78fdacb5fa83219
Pravin Mhaske <pravinmhaske817@gmail.com>
2023-09-02 15:53:22 +0530

    Section Added - carousel section added successfully.

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,12 @@
 import HeroBanner from "./heroBanner/HeroBanner";
 import "./style.scss";
+import Trending from "./trending/trending";
 
 const Home = () => {
   return (
     <div className="homePage">
       <HeroBanner />
+      <Trending/>
       <div style={{ height: 1000 }}></div>
     </div>
   );
