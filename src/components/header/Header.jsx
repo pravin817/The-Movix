f1a81be13f490e55eacf0ff89c367d5a4d68796b
Pravin Mhaske <pravinmhaske817@gmail.com>
2023-09-02 15:06:26 +0530

    Component Added - header component added auccessfully.

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,136 @@
+import React, { useState, useEffect } from "react";
+import { HiOutlineSearch } from "react-icons/hi";
+import { SlMenu } from "react-icons/sl";
+import { VscChromeClose } from "react-icons/vsc";
+import { useNavigate, useLocation } from "react-router-dom";
 import "./style.scss";
 
+import ContentWrapper from "../contentWrapper/ContentWrapper";
+import logo from "../../assets/movix-logo.svg";
+
 const Header = () => {
-  return <div>Header</div>;
+  const [show, setShow] = useState("top");
+  const [lastScrollY, setLastScrollY] = useState(0);
+  const [mobileMenu, setMobileMenu] = useState(false);
+  const [query, setQuery] = useState("");
+  const [showSearch, setShowSearch] = useState("");
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  // When we goes to next page the navbar must be at it's original position i.e (0,0)
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location]);
+
+  // Show and hide the navbar based on the scroll position
+  const controlNavbar = () => {
+    // console.log(window.scrollY);
+    if (window.scrollY > 200) {
+      if (window.scrollY > lastScrollY && !mobileMenu) {
+        setShow("hide");
+      } else {
+        setShow("show");
+      }
+    } else {
+      setShow("top");
+    }
+    setLastScrollY(window.scrollY);
+  };
+
+  useEffect(() => {
+    // Add the eventListner
+    window.addEventListener("scroll", controlNavbar);
+
+    // Remove the event Listner --> To avoid the memory leakage issue
+    return () => {
+      window.removeEventListener("scroll", controlNavbar);
+    };
+  }, [lastScrollY]);
+
+  // Function for the operating the mobile menu
+  // Open the search bar in mobile
+  const openSearch = () => {
+    setMobileMenu(false);
+    setShowSearch(true);
+  };
+
+  // Open the mobile menu
+  const openMobileMenu = () => {
+    setMobileMenu(true);
+    setShowSearch(false);
+  };
+
+  // Handle the navigation to the explore section based on clicks
+  const navigationHandler = (type) => {
+    if (type === "movie") {
+      navigate("/explore/movie");
+    } else {
+      navigate("/explore/tv");
+    }
+
+    //close the setMobileMenu to false
+    setMobileMenu(false);
+  };
+
+  const searchQueryHandler = (event) => {
+    // check for the valide input
+    if (event.key === "Enter" && query.length > 0) {
+      navigate(`/search/${query}`);
+
+      // Remove the searchBar from the screen
+      setTimeout(() => {
+        setShowSearch(false);
+      }, 500);
+    }
+  };
+
+  return (
+    <header className={`header ${mobileMenu ? "mobileView" : ""} ${show}`}>
+      <ContentWrapper>
+        <div className="logo">
+          <img src={logo} alt="" />
+        </div>
+        <ul className="menuItems">
+          <li className="menuItem" onClick={() => navigationHandler("movie")}>
+            Movies
+          </li>
+          <li className="menuItem" onClick={() => navigationHandler("tv")}>
+            TV Shows
+          </li>
+          <li className="menuItem">
+            <HiOutlineSearch onClick={openSearch} />
+          </li>
+        </ul>
+        <div className="mobileMenuItems">
+          <HiOutlineSearch onClick={openSearch} />
+
+          {/* Handle the mobile menu conditionally  */}
+          {mobileMenu ? (
+            <VscChromeClose onClick={() => setMobileMenu(false)} />
+          ) : (
+            <SlMenu onClick={openMobileMenu} />
+          )}
+        </div>
+      </ContentWrapper>
+
+      {/* SearchBar started  */}
+      {showSearch && (
+        <div className="searchBar">
+          <ContentWrapper>
+            <div className="searchInput">
+              <input
+                type="text"
+                placeholder="Search for a movie or TV show....."
+                onChange={(e) => setQuery(e.target.value)}
+                onKeyUp={searchQueryHandler}
+              />
+              <VscChromeClose onClick={() => setShowSearch(false)} />
+            </div>
+          </ContentWrapper>
+        </div>
+      )}
+    </header>
+  );
 };
 
 export default Header;
