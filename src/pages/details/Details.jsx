8d912d0474a3c860ab895d959bd22d9887daea2d
Pravin Mhaske <pravinmhaske817@gmail.com>
2023-09-05 23:58:29 +0530

    Page Added - Detail Page Added, DetailPage Banner, VideoPopup, PlayBtn added successfully.

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -1,7 +1,20 @@
 import "./style.scss";
+import { useParams } from "react-router-dom";
+import DetailsBanner from "./detailBanner/DetailBanner";
+import useFetch from "../../hooks/useFetch";
 
 const Details = () => {
-  return <div>Details</div>;
+  const { mediaType, id } = useParams();
+
+  const { data, loading } = useFetch(`/${mediaType}/${id}/videos`);
+  const { data: credits, loading: creditLoading } = useFetch(
+    `/${mediaType}/${id}/credits`
+  );
+  return (
+    <div>
+      <DetailsBanner video={data?.results?.[0]} crew ={credits?.crew} />
+    </div>
+  );
 };
 
 export default Details;
